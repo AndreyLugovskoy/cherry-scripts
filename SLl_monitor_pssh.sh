040c42d0383b8fadf8d071b3bb57964b518eee89
Andrey Lugovskoy <andrey.lugovskoy@gmail.com>
2017-04-11 12:31:24 +0300

    Add S and Z process types.

--- SLl_monitor_pssh.sh
+++ SLl_monitor_pssh.sh
@@ -3,9 +3,12 @@
 tmp_ndlist=/tmp/$RANDOM\.nd_list_sll
 sinfo --Node | tail -n +2 | grep -v 'down' | awk '{print $1}' | sort -u >> $tmp_ndlist
 
+#ps aux | grep SLl | grep -v grep
+
 # get 
-pssh_out=`pssh -h $tmp_ndlist -i 'ps aux | grep SLl | grep -v grep'`
+pssh_out=`pssh -h $tmp_ndlist -i "ps aux | grep '\([^\s]*\s\)\{7\}[S|Z|SLl]\s' | grep -v grep | grep 'vasp\|ph\|pw'"`
 rm $tmp_ndlist
+
 
 # Only success
 echo -e "$pssh_out" | sed '/FAILURE/d'

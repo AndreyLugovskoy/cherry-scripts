c930c7b77d73c8a9c47990bc3365f1bb12ece6c3
Andrey Lugovskoy <andrey.lugovskoy@gmail.com>
2017-09-29 17:51:48 +0300

    Minor correction in swapthings.sh

--- swapthings.sh
+++ swapthings.sh
@@ -4,4 +4,4 @@
 sw=$(free -g | tail -1  | awk '{print $3}')
 us=$(squeue -h -w `hostname` | awk '{ print $3 "\t" $4 }')
 
-if [ $sw -ne 0 ]; then echo $us $sw `hostname`; fi
+if [ $sw -ne 0 ]; then echo $us `hostname` $sw ; fi

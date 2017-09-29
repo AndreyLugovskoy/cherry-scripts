8e3ef85723ccd98b5db48c694af8a98b4d2d2fa3
Andrey Lugovskoy <andrey.lugovskoy@gmail.com>
2017-09-29 19:21:34 +0300

    Add eye candy

--- swap_monitor_pssh.sh
+++ swap_monitor_pssh.sh
@@ -4,15 +4,25 @@ tmp_ndlist=/tmp/$RANDOM\.nd_list_sll
 # make a list of nodes, exclude 'down' nodes
 sinfo --Node | tail -n +2 | grep -v 'down' | awk '{print $1}' | sort -u >> $tmp_ndlist
 
-echo 'Obtaining the data on swap usage.'
+echo 'Obtaining the data on swap usage..'
 
 # -i show outout # -X -T to prevent complaining about terminal
 # -I read commands from input # < /PATH/TO/SUBSCRIPT
 pssh_out=`pssh -i -X -T -h $tmp_ndlist -I < /home/andrey/scripts/slurm/slurm_around/swapthings.sh`
 
+
+bb=$(tput bold setaf 4)
+normal=$(tput sgr0)
+
 # global header
-echo  'Job_name User  Node  Swap,Gb'
+
+#TODO: add nothing to report case
+
+printf '%15.10s %15.10s %15.10s %15.10s\n'  "Job_name" 'User'  'Node'  'Swap,Gb'
+
+
+pssh_out=`echo -e "$pssh_out" | sed '/FAILURE/d' | sed '/SUCCESS/d'`
 
 # do something with output (clean, sort, mulitply by e^x, whatever)
-echo -e "$pssh_out" | sed '/FAILURE/d' | sed '/SUCCESS/d'
+printf '%15.10s %15.10s %15.10s %15.10s\n' $pssh_out; 
 
